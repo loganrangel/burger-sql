15ac4a32cc9b903a7742f53e00ff3b4486196341
Logan Angel <you@example.com>
2019-06-21 12:54:21 -0400

    updated connections

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured) 
+    VALUES ('all-american', true);
+INSERT INTO burgers(burger_name, devoured)
+    VALUES ('black-and-bleu', false);
+INSERT INTO burgers(burger_name, devoured)
+    VALUES ('smokehouse', true);
---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
